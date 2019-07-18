e4d17a262c0479e7ada95140166bc981db9c58d0
Toua Thao <touathao09@gmail.com>
2019-07-18 15:36:42 -0500

    added more things

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            byte number = 2;
+            int count = 19;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Toua";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
             Console.WriteLine("Hello World!");
+            Console.WriteLine("{0},{1}",byte.MinValue,byte.MaxValue);
+            Console.WriteLine("{0},{1}",float.MaxValue,float.MinValue);
         }
     }
 }
