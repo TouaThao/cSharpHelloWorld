15bf04a6f6ecee2ab52dd0f59bc88b0b43db380c
Toua Thao <touathao09@gmail.com>
2019-07-23 13:38:47 -0500

    added more stuff

--- Program.cs
+++ Program.cs
@@ -6,21 +6,56 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 19;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Toua";
-            bool isWorking = true;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("{0},{1}",byte.MinValue,byte.MaxValue);
-            Console.WriteLine("{0},{1}",float.MaxValue,float.MinValue);
+
+            // try
+            // {
+            //     var testingString = "1234";
+            //     byte b = Convert.ToByte(testingString);
+            // }
+            // catch (System.Exception)
+            // {
+            //     Console.WriteLine("Cannot convert the string to byte");
+            // }
+            // var intergerA = 12;
+            // var intergerB = 5;
+            // var newNumber = "1234";
+            // int i = Convert.ToInt32(newNumber);
+            // byte number = 2;
+            // int count = 19;
+            // float totalPrice = 20.95f;
+            // char character = 'A';
+            // string firstName = "Toua";
+            // bool isWorking = true;
+            // Console.WriteLine("Operator Add" + " "+ intergerA + intergerB);
+            // Console.WriteLine("Operator Division" + " "+ (float)intergerA / (float)intergerB);
+            // Console.WriteLine(newNumber);
+            // Console.WriteLine(number);
+            // Console.WriteLine(count);
+            // Console.WriteLine(totalPrice);
+            // Console.WriteLine(character);
+            // Console.WriteLine(firstName);
+            // Console.WriteLine(isWorking);
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("{0},{1}", byte.MinValue, byte.MaxValue);
+            // Console.WriteLine("{0},{1}", float.MaxValue, float.MinValue);
+
+            string[] dayOfTheWeek ={
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+        }
+    public class console {
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
+
+    }
+
 }
